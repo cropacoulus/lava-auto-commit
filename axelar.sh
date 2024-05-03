2ea1585dbfc0f4af1fd55313119aab7af2b8013c
Muhammad Rifki <72106126+cropacoulus@users.noreply.github.com>
2024-05-03 11:20:02 +0700

    Update axelar.sh

--- axelar.sh
+++ axelar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://tm.axelar.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://tm.axelar.lava.build/lava-referer-7fe1fbe0-6f08-4e62-9f64-8d6c6f84c8c0/"
 
 OUTPUT_DIR="logs"
 
